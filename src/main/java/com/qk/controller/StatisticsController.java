fbab899b6589efc88776a5b8a29b8083622b042e
35890 <35890226@qq.com>
2022-06-20 19:28:01 +0800

    更新项目

---
+++ src/main/java/com/qk/controller/StatisticsController.java
@@ -0,0 +1,29 @@
+package com.qk.controller;
+
+import com.qk.entity.BookInfo;
+import com.qk.service.BookInfoService;
+import com.qk.service.TypeInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class StatisticsController {
+
+    @Autowired
+    private BookInfoService bookInfoService;
+
+    @Autowired
+    private TypeInfoService typeInfoService;
+
+    @GetMapping("statisticIndex")
+    public String statistics(Model model){
+        //根据图书类型查询图书数量
+        List<BookInfo> list = bookInfoService.getBookCountByType();
+        model.addAttribute("list",list);
+        return "count/statisticIndex";
+    }
+}
