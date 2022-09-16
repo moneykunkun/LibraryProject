38b82e04ad3ca809bf945f24be8f1b814f3c7df2
35890 <35890226@qq.com>
2022-09-16 15:50:33 +0800

    更新项目

---
+++ src/main/java/com/yx/entity/TypeInfo.java
@@ -0,0 +1,47 @@
+package com.yx.entity;
+
+import java.io.Serializable;
+
+public class TypeInfo implements Serializable {
+
+    private Integer id;
+
+
+    private String name;
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+
+
+    private String remarks;
+
+
+    private static final long serialVersionUID = 1L;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+
+}
